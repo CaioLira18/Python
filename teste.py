95a29cd79596144ca359e33307db99f9e621044b
Jeronimo Barbosa <jbr2@cesar.school>
2024-05-07 13:44:52 -0300

    Menu interativo atualizado - novas func - modelo novo

--- teste.py
+++ teste.py
@@ -13,8 +13,31 @@ def adicionar_receita():
 
 # Menu Interativo   
 while True: # Loop While para o usuario ter a possibilidade de realizar outra função
-    opcao = input("Opções de Entrada:\n1 - Adicionar \n2 - Remover\n3 - Atualizar\n4 - Visualizar\n5 - Filtrar para o país\n")
+    opcao = input("Opções de Entrada:\n1 - Funções Básicas (Adicionar, Remover, Visualizar e Atualizar)\n2 - Funções especiais (Filtrar por país, Lista de favoritos, Receitas aleatórias e Filtrar por tipo de prato)\n3 - Finalizar\n\nDigite o numero referente a operação desejada:")
 
     if opcao == '1':
-        adicionar_receita() # Chama a função de 'Adicionar Receita'
-   
+        opcao = input("Opções de Entrada:\n1 - Adicionar \n2 - Remover\n3 - Atualizar\n4 - Visualizar\n5 - Voltar para início\n\nDigite o numero referente a operação desejada: ")
+        if opcao == '1':
+            adicionar_receita()
+        elif opcao == '2':
+            remover_receita()
+        elif opcao == '3':
+            atualizar_receita()
+        elif opcao == '4':
+            visualizar_receita()
+        elif opcao == '5':
+            continue
+    elif opcao == '2':
+        opcao = input("Opções de Entrada:\n1 - Filtrar por país \n2 - Lista de favoritos\n3 - Receitas aleatórias\n4 - Filtrar por tipo de prato\n5 - Voltar para início\n\nDigite o numero referente a operação desejada: ")
+        if opcao == '1':
+            filtrar_pais()
+        elif opcao == '2':
+            lista_favoritos()
+        elif opcao == '3':
+            receitas_aleatorias()
+        elif opcao == '4':
+            filtrar_prato()
+        elif opcao == '5':
+            continue
+    elif opcao == '3':
+        break
